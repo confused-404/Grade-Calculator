d52f8b94f7f8296c56e85a1dd12021ebcd0b32e5
Yves Kloeckner <hosta.gruen@gmail.com>
2024-04-26 17:26:18 -0600

    fixed deprecated code

--- root/backend/grade_calculator/src/main.rs
+++ root/backend/grade_calculator/src/main.rs
@@ -1,10 +1,8 @@
-use axum::cors::Cors;
 use axum::{
-    http::{self, Method},
-    routing::{get, post},
+    http::self,
+    routing::post,
     Router,
 };
-use http::header::CONTENT_TYPE;
 
 use crate::handlers::calculate_grade;
 
@@ -13,17 +11,11 @@ mod models;
 
 #[tokio::main]
 async fn main() {
-    let app: Router = Router::new().route("/calculate", post(calculate_grade)); // TODO: change this
-
-    let cors = Cors::new()
-        .allow_methods(vec![http::Method::POST]) // Allow only POST method
-        .allow_origin("*") // Allow any origin
-        .allow_headers(vec!["content-type"]); // Allow specific headers
-
-    let app = app.layer(cors);
+    let app: Router = Router::new()
+        .route("/calculate", post(calculate_grade)); // TODO: change this
 
     println!("Running on http://localhost:8080");
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app.into_make_service()).await.unwrap();
 }
