5abb4f03d1b15d8feb5e4c8761ee0d79e02df8ae
Yves Kloeckner <hosta.gruen@gmail.com>
2024-04-26 21:36:05 -0600

    backend hosted and working

--- root/backend/grade_calculator/src/main.rs
+++ root/backend/grade_calculator/src/main.rs
@@ -1,6 +1,5 @@
 use axum::{
-    routing::post,
-    Router,
+    response::Html, routing::{get, post}, Router
 };
 
 use crate::handlers::calculate_grade;
@@ -11,10 +10,15 @@ mod models;
 #[tokio::main]
 async fn main() {
     let app: Router = Router::new()
-        .route("/calculate", post(calculate_grade)); // TODO: change this
+        .route("/calculate", post(calculate_grade))
+        .route("/", get(test_main));
 
     println!("Running on http://localhost:8080");
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app.into_make_service()).await.unwrap();
+}
+
+async fn test_main() -> Html<&'static str> {
+    Html("<h1>Hello World!</h1>")
 }
